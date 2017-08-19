aced12322b844c570937044ea56359a238e06f44
Knut Jackowski <doxanthropos@googlemail.com>
2017-08-19 14:04:57 +0200

    First version

--- assets/main.js
+++ assets/main.js
@@ -1,5 +1,15 @@
 $(function() {
-
-  // your code will go here
+  var myReportcard = "https://www.codeschool.com/users/knutj.json";
+  $.ajax({
+    url: myReportcard,
+    dataType: 'jsonp',
+    success: function(response){
+      response.courses.completed.forEach(function(element){
+        $('#badges').append("<div class='course'><h3>" +  element.title + "</h3>"+
+        "<img src='"+ element.badge +"' /><a href='" +
+        element.url + "' target='_blank' class='btn btn-primary'>See Course</a></div>")
+      })
+    }
+  });
 
 });
